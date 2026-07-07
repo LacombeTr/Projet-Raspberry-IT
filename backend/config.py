249b7c867039176cae2b69bdea6eac5a4ccd4515
Tristan Lacombe <tlacombevulca@outlook.fr>
2026-07-07 13:35:44 +0200

    feat(app): Added env variables

---
+++ backend/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    NASA_FIRMS_API_KEY: str
+    OPENWEATHERMAP_API_KEY: str
+    FIRE_RADIUS_KM: int
+    FIRMS_URL: str
+    FIRMS_SOURCE: str
+    LATITUDE: float
+    LONGITUDE: float
+
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="allow"
+    )
+
+settings = Settings()
